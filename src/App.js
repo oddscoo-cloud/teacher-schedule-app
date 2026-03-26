a3e327c56a5cd50c97be9a7b3e69df72e4b4c90b
oddscoo-cloud <oddscoo@gmail.com>
2026-03-26 18:47:40 +0200

    Animated and styled some components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import MainContent from "./components/MainContent/MainContent";
-import Footer from "./components/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <MainContent />
-    </>
+    </div>
   );
 }
 
--- src/data/users.js
+++ src/data/users.js
@@ -1,4 +1,3 @@
-
 export const users = [
   { id: 1, role: "admin", username: "superadmin", password: "1234" },
   { id: 2, role: "teacher", username: "oleg", password: "oleg" },
